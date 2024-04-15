29f2a369e7e5d96f3ca3789f2b5a87ce98aa4101
Sean McFall <sean.l.mcfall@gmail.com>
2024-04-15 01:47:59 -0400

    vim incremental search

--- .config/nvim/lua/sean/core/options.lua
+++ .config/nvim/lua/sean/core/options.lua
@@ -18,6 +18,7 @@ opt.wrap = false -- disable line wrapping
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.incsearch = true
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
