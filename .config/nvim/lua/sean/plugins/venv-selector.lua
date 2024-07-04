75a3532eeb6598f23da27a2cb34216f1ba7d99a2
Sean McFall <sean.l.mcfall@gmail.com>
2024-07-03 23:44:23 -0400

    tmux theme; venv-selector

--- .config/nvim/lua/sean/plugins/venv-selector.lua
+++ .config/nvim/lua/sean/plugins/venv-selector.lua
@@ -1,21 +1,15 @@
 return {
-  'linux-cultist/venv-selector.nvim',
+  "linux-cultist/venv-selector.nvim",
   dependencies = {
-    'neovim/nvim-lspconfig',
-    'nvim-telescope/telescope.nvim',
-    'mfussenegger/nvim-dap-python'
+    "neovim/nvim-lspconfig",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
+  lazy = false,
+  branch = "regexp", -- This is the regexp branch, use this for the new version
   config = function()
-    require('venv-selector').setup {
-      parents = 1,
-      poetry_path = '/Users/seanmcfall/Library/Caches/pypoetry/virtualenvs',
-    }
+    require("venv-selector").setup()
   end,
-  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
   keys = {
-    -- Keymap to open VenvSelector to pick a venv.
     { '<leader>vs', '<cmd>VenvSelect<cr>' },
-    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
   },
 }
