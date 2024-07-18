35ae99ee77d6f20179dd17933ee8da6cd6a0392d
Sean McFall <sean.l.mcfall@gmail.com>
2024-07-18 00:04:51 -0400

    telescope find file name display

--- .config/nvim/lua/sean/plugins/telescope.lua
+++ .config/nvim/lua/sean/plugins/telescope.lua
@@ -11,9 +11,26 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "TelescopeResults",
+      callback = function(ctx)
+        vim.api.nvim_buf_call(ctx.buf, function()
+          vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+          vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+        end)
+      end,
+    })
+
+    local function filenameFirst(_, path)
+      local tail = vim.fs.basename(path)
+      local parent = vim.fs.dirname(path)
+      if parent == "." then return tail end
+      return string.format("%s\t\t%s", tail, parent)
+    end
+
     telescope.setup({
       defaults = {
-        path_display = { "smart" },
+        path_display = filenameFirst,
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
