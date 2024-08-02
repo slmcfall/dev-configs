085b364bad5f289b2449ec2001232b0c65484963
Sean McFall <sean.l.mcfall@gmail.com>
2024-08-02 19:54:15 -0400

    bbq; colors

--- .config/nvim/lua/sean/plugins/barbecue.lua
+++ .config/nvim/lua/sean/plugins/barbecue.lua
@@ -2,13 +2,14 @@ return {
   {
     "utilyre/barbecue.nvim",
     name = "barbecue",
-    event = "VeryLazy",
+    event = "BufWinEnter",
     version = "*",
     dependencies = {
       "SmiteshP/nvim-navic",
       "nvim-tree/nvim-web-devicons", -- optional dependency
     },
     opts = {
+      include_buftypes = { "", "acwrite" },
       show_navic = false,
       show_modified = true,
       symbols = {
