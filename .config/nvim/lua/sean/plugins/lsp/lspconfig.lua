305dbc4b55b3d15aca3ef771094c962a4e164b40
slmcfall <sean.mcfall@mindoula.com>
2024-09-24 12:56:03 -0400

    gD vsplit

--- .config/nvim/lua/sean/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/sean/plugins/lsp/lspconfig.lua
@@ -40,7 +40,11 @@ return {
         keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
         opts.desc = "Go to declaration"
-        keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+        -- keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+        keymap.set("n", "gD", function()
+          vim.cmd("vsplit")
+          vim.lsp.buf.declaration()
+        end, opts)
 
         opts.desc = "Show LSP definitions"
         keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
