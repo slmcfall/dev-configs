4fbf98bde1ab29bd47ec50b0e75d2a26dfb36799
Sean McFall <sean.l.mcfall@gmail.com>
2024-10-18 17:40:16 -0400

    blink theme fix

--- .config/nvim/lua/sean/plugins/blink.lua
+++ .config/nvim/lua/sean/plugins/blink.lua
@@ -4,10 +4,11 @@ return {
   enabled = true,
   -- optional: provides snippets for the snippet source
   dependencies = {
-    "L3MON4D3/LuaSnip",
+    -- "garymjr/nvim-snippets",
+    -- "L3MON4D3/LuaSnip",
     "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim",         -- vs-code like pictograms
-    -- -- "saadparwaiz1/cmp_luasnip",           -- for autocompletion
+    -- "onsails/lspkind.nvim",         -- vs-code like pictograms
+    -- "saadparwaiz1/cmp_luasnip",           -- for autocompletion
     -- "hrsh7th/cmp-buffer",      -- source for text in buffer
     -- "hrsh7th/cmp-path",        -- source for file system paths
     -- "SergioRibera/cmp-dotenv", -- environment variables
@@ -16,6 +17,40 @@ return {
   },
   version = 'v0.*',
   opts = {
+    highlight = {
+      -- sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release, assuming themes add support
+      use_nvim_cmp_as_default = true,
+    },
+    providers = {
+      { "blink.cmp.sources.lsp", name = "LSP" },
+      {
+        "blink.cmp.sources.snippets",
+        name = "Snippets",
+        score_offset = -3,
+        -- similar to https://github.com/garymjr/nvim-snippets
+        opts = {
+          friendly_snippets = false,
+          search_paths = { vim.fn.stdpath("config") .. "/lua/sean/plugins/snippets" },
+          global_snippets = { "all" },
+          extended_filetypes = {},
+          ignored_filetypes = {},
+        },
+      },
+      {
+        "blink.cmp.sources.path",
+        name = "Path",
+        score_offset = 3,
+        opts = { get_cwd = vim.uv.cwd },
+      },
+      {
+        "blink.cmp.sources.buffer",
+        name = "Buffer",
+        keyword_length = 3,
+        fallback_for = { "LSP" }, -- PENDING https://github.com/Saghen/blink.cmp/issues/122
+      },
+    },
     keymap = {
       accept = '<CR>',
       show_documentation = '<C-p>',
@@ -28,7 +63,8 @@ return {
     windows = {
       autocomplete = {
         winhighlight =
-        'Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorColumn:BlinkCmpMenu,Search:BlinkCmpMenuBorder'
+        'bg:BlinkCmpMenu,bg:BlinkCmpMenuBorder,bg:BlinkCmpMenu,bg:BlinkCmpMenuBorder',
+        border = 'padded'
       },
       documentation = {
         min_width = 10,
@@ -42,5 +78,32 @@ return {
     accept = { auto_brackets = { enabled = true } },
     -- experimental signature help support
     trigger = { signature_help = { enabled = true } },
+    kind_icons = {
+      Text = "󰊄",
+      Method = "󰊕",
+      Function = "󰊕",
+      Constructor = "",
+      Field = "󰇽",
+      Variable = "󰂡",
+      Class = "⬟",
+      Interface = "",
+      Module = "",
+      Property = "󰜢",
+      Unit = "",
+      Value = "󰎠",
+      Enum = "",
+      Keyword = "󰌋",
+      Snippet = "󰒕",
+      Color = "󰏘",
+      Reference = "",
+      File = "󰉋",
+      Folder = "󰉋",
+      EnumMember = "",
+      Constant = "󰏿",
+      Struct = "",
+      Event = "",
+      Operator = "󰆕",
+      TypeParameter = "󰅲",
+    },
   },
 }
